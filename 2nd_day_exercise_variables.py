81ff11195829518ec7422c1fd62da0e37c68228b
Ieva Zarina <ieva.zarina91@gmail.com>
2022-09-12 13:35:49 +0300

    First attempt of homework

--- 2nd_day_exercise_variables.py
+++ 2nd_day_exercise_variables.py
@@ -1,3 +1,37 @@
+my_name = "Ieva"
+print(type(my_name))
+print(my_name)
+my_num = 30
+print(type(my_num))
+
+a = 42
+b = 42
+c = 43
+print(id(a))
+print(id(a), id(b), id(c), id(my_num))
+print(id(c)-id(a))
+
+print(a, b, my_num)
+b = 9001
+c = 9_000
+print(a, b, c, my_num)
+print(a, b, c, my_num, sep="\n")
+print(a, b, c, my_num, sep="\n***\n")
+
+c = c + 5000
+print(c)
+c += 10_000
+print(c)
+
+c = c + 3.14
+print(c, type(c))
+
+c = c - 3.14
+print(c, type(c))
+
+c = int(c)
+print(c, type(c))
+
 # Write a program that asks for and saves a username
 # Ask a question about the user's age, using the username in the question.
 # Shows in how many years the user will be 100 years old smile
