1ac88f4d5beff9e73aab6b5f1317dcb80f246d6d
Ieva Zarina <ieva.zarina91@gmail.com>
2022-09-13 18:35:57 +0300

    Practising_booleans

---
+++ Day3_theory_booleans.py
@@ -0,0 +1,50 @@
+# # Booleans
+# is_raining = False
+# is_sunny = True
+# is_summer = False
+# print(is_raining, is_sunny)
+# print(type(is_raining), type(is_sunny), type(is_summer))
+# print(2 * 2 == 4)
+# math = 2 * 2 == 4
+# print(math)
+# a = 2
+# b = 4
+# print(a * a == b, a * a == 4, a * a == 5)
+# print(2 == 2.0, a == 2.0)
+# print(1 == True, False == 0)
+# print("" == False)
+# print("" == "")
+# my_name = "Ieva"
+# print(my_name == my_name, my_name == "Ieva", my_name == "IEVA")
+# print("Ieva" == 30, my_name == 3050)
+# print(a != b)
+# print(a != 2, 2 != 2)
+# not_equal = 2 * a != b
+# print(not_equal)
+# print(2 > 3, 3 > 2, 9001 > 9000)
+# print(a * a > 3, a * a > b, a * a > b * 15)
+# print("Ieva" > "Iava")
+# print(ord("I"), ord("e"), ord("a"))
+# print(chr(101), chr(97), chr(1040))
+# print(len("Ievina") > len("Ieva"))
+# print(len("Ievina"))
+# print(len("Ieva"))
+# print(2 <= 3, 3 <= 3, 5 <= 3)
+# print(a*2 >= b, a*2 <= b*2)
+# print(a is b)
+# print(id(a) == id(b))
+
+#LOGIC
+# is_sun = True
+# print(is_sun)
+# is_sun = not is_sun
+# print(is_sun)
+# is_sun = not is_sun
+# print(is_sun)
+# print(True and True)
+# print(True and False)
+
+#CONDITIONALS
+if True:
+    print("True")
+    print("This always runs")
